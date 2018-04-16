cb2c378a539b05ea4f257717b122e9ef6637df1a
PatelProjects <38337132+PatelProjects@users.noreply.github.com>
2018-04-16 12:40:38 -0400

    Add files via upload
    Secondary button and updated interface

--- Website Speed Test Extension/background.js
+++ Website Speed Test Extension/background.js
@@ -1,14 +1,19 @@
-chrome.browserAction.onClicked.addListener(function(activeTab){
+document.addEventListener('DOMContentLoaded', function() {
+	var checkButton = document.getElementById('checkButton');
+	checkButton.addEventListener('click', function() {
+		chrome.tabs.query({ currentWindow: true, active: true }, function(tabs) {
+			var newURL = tabs[0].url;
 
-chrome.tabs.query({currentWindow: true, active: true}, function(tabs){
-     var newURL = tabs[0].url;
+			var url =
+				'https://developers.google.com/speed/pagespeed/insights/?url=https%3A%2F%2F';
 
-var url = "https://developers.google.com/speed/pagespeed/insights/?url=https%3A%2F%2F";
+			var newURLL;
 
-var newURLL;
+			newURLL = url.concat(
+				newURL.slice(newURL.indexOf('//') + 2, newURL.indexOf('/', 8) + 1)
+			);
 
-newURLL = url.concat(newURL.slice(newURL.indexOf('//')+2,newURL.indexOf('/',8 ) +1));
-
-chrome.tabs.create({ url: newURLL});
-});
+			chrome.tabs.create({ url: newURLL });
+		});
+	});
 });
